d5ba83307684c40e0806419de309d5282f9d4f10
bombom cho <drjreric@gmail.com>
2023-11-13 16:03:13 +0900

    test

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import helloAPI, randomQuiz, ImageViewSet
 
 urlpatterns = [
-    path("hello/", helloAPI),
+    # path("hello/", helloAPI),
     path("<int:id>/", randomQuiz),
     path("upload/", ImageViewSet.as_view(), name='upload'),
 ]
